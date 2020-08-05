342307e49ce40f38bbae2a06e56ffa252fbbc32e
mooresv <mooresv@ornl.gov>
2020-08-05 05:28:53 -0400

    Update createObservable_ex.cpp

--- example_code/createObservable_ex.cpp
+++ example_code/createObservable_ex.cpp
@@ -1,3 +1,8 @@
+// The QCOR implementation code below in inconsistent with the spec. The spec creates the observable in two steps:
+//   Observable qcor::createPauli(std::map<int,string> sitemap, std::complex coef, string paulistr)
+//
+//   Observable* qcor::createObservable (Observable &obsvOp, FieldOperator &fieldOp)
+//
 // Create the Dueteron Hamiltonian (Observable)
 auto H = qcor::createObservable(
       "5.907 - 2.1433 X0X1 - 2.1433 Y0Y1 + .21829 Z0 - 6.125 Z1");
