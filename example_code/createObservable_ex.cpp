e75f450e506aa1f10d3e6c895adcf998bceb2f33
mooresv <mooresv@ornl.gov>
2020-08-05 04:56:45 -0400

    Update createObservable_ex.cpp

--- example_code/createObservable_ex.cpp
+++ example_code/createObservable_ex.cpp
@@ -1,2 +1,3 @@
-//This is a place holder
-//Replace with real example
+// Create the Dueteron Hamiltonian (Observable)
+auto H = qcor::createObservable(
+      "5.907 - 2.1433 X0X1 - 2.1433 Y0Y1 + .21829 Z0 - 6.125 Z1");
