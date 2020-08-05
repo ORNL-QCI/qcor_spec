f4ae20faab36bcb717a323096ca5dc21e5b5b689
mooresv <mooresv@ornl.gov>
2020-08-05 05:12:37 -0400

    Update taskInitiate_ex.cpp

--- example_code/taskInitiate_ex.cpp
+++ example_code/taskInitiate_ex.cpp
@@ -1,3 +1,9 @@
+// The code below from the QCOR implementation is inconsistent with the QCOR spec in that:
+// - the arguments are different
+// - spec says
+//     Handle qcore::taskInitiate(Observable *observable, Kernel *kernel, Objective *objective, Optimizer *optimizer)
+// - implementation has observable as an argument to createObjectiveFunction
+
 // Call taskInitiate, kick off optimization of the given
 // functor dependent on the ObjectiveFunction, async call
   auto handle = qcor::taskInitiate(
