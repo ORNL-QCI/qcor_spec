0b094e2abcf412bf0c090132e70b133c83744b41
mooresv <mooresv@ornl.gov>
2020-08-05 05:01:09 -0400

    Update taskInitiate_ex.cpp

--- example_code/taskInitiate_ex.cpp
+++ example_code/taskInitiate_ex.cpp
@@ -1,2 +1,13 @@
-//This is a place holder
-//Replace with real example
+// Call taskInitiate, kick off optimization of the given
+// functor dependent on the ObjectiveFunction, async call
+  auto handle = qcor::taskInitiate(
+      objective, optimizer,
+      [&](const std::vector<double> x, std::vector<double> &dx) {
+        return (*objective)(q, x[0]);
+      },
+      1);
+
+  // Go do other work...
+
+  // Query results when ready.
+  auto results = qcor::sync(handle);
