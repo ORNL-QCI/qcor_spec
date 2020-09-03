e002efc8d2f0ac5bb69f3eb669620549b5172f64
Shirley Moore <mooresv@ornl.gov>
2020-09-03 12:44:28 -0400

    Update taskInitiate_ex.cpp
    added ansatz to example

--- example_code/taskInitiate_ex.cpp
+++ example_code/taskInitiate_ex.cpp
@@ -1,7 +1,25 @@
 // The QCOR implementation is inconsistent with the QCOR spec in that:
-// - the arguments are different
+// - the arguments are different for createObjectiveFunction
 //
 // - 
+__qpu__ void ansatz(qreg q, double theta) {
+  X(q[0]);
+  Ry(q[1], theta);
+  CX(q[1], q[0]);
+}
+
+ // Allocate 2 qubits
+  auto q = qalloc(2);
+
+  // Create the Deuteron Hamiltonian
+  auto H = 5.907 - 2.1433 * X(0) * X(1) - 2.1433 * Y(0) * Y(1) + .21829 * Z(0) -
+           6.125 * Z(1);
+
+  // Create the ObjectiveFunction
+  auto objective = qcor::createObjectiveFunction(ansatz, H, q, 1);
+
+  // Create the Optimizer
+  auto optimizer = qcor::createOptimizer("nlopt");
 
 // Call taskInitiate, kick off optimization of the given
 // ObjectiveFunction, asynchronous call
